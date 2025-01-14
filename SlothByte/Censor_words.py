db8a7cc7a34f1673bd5d7bed1a7a0cd4d2974289
Juul <juul20018@gmail.com>
2025-01-14 19:29:56 +0100

    little change to censor word challenge

--- SlothByte/Censor_words.py
+++ SlothByte/Censor_words.py
@@ -3,8 +3,7 @@ def censor(s):
     
     for i, word in enumerate(words):
         if len(word) > 4:
-            censored = ['*' for j in range(0,len(word))]
-            words[i] = ''.join(str(x) for x in censored)
+            words[i] = ''.join(str(x) for x in ['*' for j in range(0,len(word))])
     
     censored_s = ' '.join(str(i) for i in words)
     return censored_s
