f3dfea5bb66c8bd8e0a8f1a6f8db5bdb19201d40
Juul <juul20018@gmail.com>
2025-02-12 16:43:37 +0100

    finishing removing virusses slothbyte challenge

--- SlothByte/rm_comp_virus.py
+++ SlothByte/rm_comp_virus.py
@@ -1,11 +1,18 @@
 def remove_virus(files):
     list_files = files[10:].split(', ')
-    print(list_files)
     protected = ['notvirus', 'antivirus']
 
-    for f in list_files:
+    for i, f in enumerate(list_files):
         if ('virus' in f or 'malware' in f) and (protected[0] not in f and protected[1] not in f):
-            print("virus found")
+            list_files[i] = 0;
+
+    new_list = [i for i in list_files if i != 0]
+
+    if (len(new_list) != 0):
+        res = ', '.join(new_list)
+        print("PC Files: " + res)
+    else:
+        print("PC Files: Empty")
 
 if __name__ == "__main__":
     remove_virus("PC Files: spotifysetup.exe, virus.exe, dog.jpg")
